10d8214c70a26b323133b64535068a7057f304f6
Deivaprakash <deivaprakash2003@gmail.com>
2024-08-17 18:51:13 +0530

    int to float conversion

--- Distance_to_goal/euclidean_distance.py
+++ Distance_to_goal/euclidean_distance.py
@@ -1,12 +1,12 @@
 import math
 import sys
 
-x1,y1 = (int(sys.argv[1]),int(sys.argv[2]))
-x2,y2 = (int(sys.argv[3]),int(sys.argv[4]))
+x1,y1 = (float(sys.argv[1]),float(sys.argv[2]))
+x2,y2 = (float(sys.argv[3]),float(sys.argv[4]))
 
 def euclidean_distance(x1, y1, x2, y2):
     euclidean_distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    print(f"Euclidean distance between {x1,y1} and {x2,y2} is {euclidean_distance: .3f}")
+    print(f"Euclidean distance between {x1,y1} and {x2,y2} is {euclidean_distance:.3f}")
     return euclidean_distance
 
 if __name__=='__main__':
